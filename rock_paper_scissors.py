334efe5a0e567854a5a54f1dfcc5d17a562efb84
charmi surati <charmisurati1@gmail.com>
2018-08-07 13:25:20 +0530

    First commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,6 +14,17 @@ def play():
     i = random.randint(1,3)
     c = switcher.get(i)
     print("The Computer has Selected: " +c)
+
+    if p==1 and c==3:
+        print('you win against the computer \n')
+    elif p==3 and c==1:
+        print('you lose against the computer \n')
+    elif p>c:
+        print('you win against the computer \n ')
+    elif p<c:
+        print('you lose against the computer \n')
+    elif p==c:
+        print( 'you tie against the computer\n')
     
     
 play()
